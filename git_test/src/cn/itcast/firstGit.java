c79b5ce19bf7627bc9ac6adc6f80f481ddc015c4
NOON <noon@itcast.cn>
2019-02-22 21:44:26 +0800

    java

---
+++ git_test/src/cn/itcast/firstGit.java
@@ -0,0 +1,11 @@
+package cn.itcast;
+
+public class firstGit {
+    public static void main(String[] args) {
+        System.out.println("hello git properties");
+        System.out.println("hello git properties");
+        System.out.println("hello git properties");
+        System.out.println("hello git properties");
+        System.out.println("hello git properties");
+    }
+}
